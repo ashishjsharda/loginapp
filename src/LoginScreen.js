c8801d7fb1c5f7f40f22d7483b27c68e0d2073af
Ashish Sharda <ashishjsharda@gmail.com>
2023-04-18 05:10:55 -0500

    add login screen

---
+++ src/LoginScreen.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const LoginScreen = () => {
+ const [userName, setUserName] = React.useState("");
+ const [password, setPassword] = React.useState("");
+ const [error, setError] = React.useState("");
+
+ const handleSubmit = (event) => {
+    event.preventDefault();
+    if (userName === "admin" && password === "admin") {
+      console.log("Login successful with ${userName} and ${password}")
+    } else {
+      setError("Invalid username or password");
+    }
+ }
+
+ return (
+    <div>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username">Username</label>
+                <input type={"text"}  value={userName} onChange={(event) => setUserName(event.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type={"password"} value={password} onChange={(event) => setPassword(event.target.value)} />
+            </div>
+            <br />
+            <button type="submit">Login</button>
+        </form>
+    </div>
+ )
+}
